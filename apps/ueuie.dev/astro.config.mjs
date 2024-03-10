ed23f7685b5182cfa87b6de770bb755f0a890d0b
Alex Tocar <alex.tocar@ueuie.dev>
2024-03-10 18:19:02 +0100

    ref: consise templating and code separation

--- apps/ueuie.dev/astro.config.mjs
+++ apps/ueuie.dev/astro.config.mjs
@@ -1,33 +1,24 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
-import partytown from "@astrojs/partytown";
-import mdx from "@astrojs/mdx";
+import cloudflare from '@astrojs/cloudflare';
+import partytown from '@astrojs/partytown';
+import mdx from '@astrojs/mdx';
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  site: 'https://www.ueuie.dev',
+  output: 'hybrid',
   adapter: cloudflare({
-    imageService: "passthrough",
+    imageService: 'compile',
     runtime: {
-      mode: "local",
-      type: "pages",
+      mode: 'local',
+      type: 'pages',
     }
   }),
-  site: "https://www.ueuie.dev",
-  // i18n: {
-  //   locales: ["de", "en", "ua", "ru"],
-  //   defaultLocale: "en",
-  //   fallback: {
-  //     "ru": "en",
-  //     "ua": "en",
-  //     "de": "en"
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   }
-  // },
-
-  integrations: [partytown(), mdx(), solidJs()]
+  integrations: [partytown(), mdx(), solidJs()],
+  cacheDir: '.astro',
+  experimental: {
+    contentCollectionCache: true
+  },
 });
